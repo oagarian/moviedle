a847101d71f17355f3a16afcd961c9b815b7a46c
oagarian <tns6@aluno.ifal.edu.br>
2024-06-27 10:05:28 -0300

    feat: movie repository

---
+++ src/infra/repository/postgres/movie.go
@@ -0,0 +1,86 @@
+package postgres
+
+import (
+	"fmt"
+	"moviedle/src/core/domain/errors"
+	"moviedle/src/core/domain/movie"
+	"moviedle/src/core/messages"
+	"moviedle/src/core/ports/secondary"
+	"moviedle/src/infra/repository"
+	"moviedle/src/infra/repository/postgres/query"
+	"strconv"
+
+	"github.com/google/uuid"
+)
+
+type movieRepository struct{}
+
+func NewMovieRepository() secondary.MoviePort {
+	return &movieRepository{}
+}
+
+func (g *movieRepository) Get(movieID *uuid.UUID) (movie.Movie, errors.Error) {
+	rows, err := repository.Queryx(query.NewMovieQuery().Get(), movieID.String())
+	if err != nil {
+		return nil, errors.NewUnexpected()
+	}
+	defer rows.Close()
+
+	if !rows.Next() {
+		err := errors.NewFromString(messages.MovieNotFoundErrorMessage)
+		return nil, err
+	}
+	
+	var serializedMovie = map[string]interface{}{}
+	nativeErr := rows.MapScan(serializedMovie)
+	if nativeErr	!= nil {
+        return nil, errors.NewUnexpected()
+    }
+	return newMovieFromMapRows(serializedMovie)
+}
+
+func newMovieFromMapRows(data map[string]interface{}) (movie.Movie, errors.Error) {
+	var id uuid.UUID
+	if parsedID, err := uuid.Parse(string(data["id"].([]uint8))); err != nil {
+		return nil, errors.NewUnexpected()
+	} else {
+		id = parsedID
+	}
+	
+	title := fmt.Sprint(data["title"])
+	year := fmt.Sprint(data["year"])
+	parsedYear, err := strconv.Atoi(year)
+	if err != nil {
+		return nil, errors.NewUnexpected()
+	}
+	slogan := fmt.Sprint(data["slogan"])
+	resume := fmt.Sprint(data["resume"])
+	coverURL := fmt.Sprint(data["cover_url"])
+
+	_directors, dirErr := NewDirectorRepository().ListByMovieID(&id)
+	if dirErr != nil {
+        return nil, errors.NewUnexpected()
+    }
+
+	_genres, genErr := NewGenreRepository().ListByMovieID(&id)
+	if genErr != nil {
+		return nil, errors.NewUnexpected()
+	}
+
+	_oscars, oscErr := NewOscarRepository().ListByMovieID(&id)
+	if oscErr != nil {
+        return nil, errors.NewUnexpected()
+    }
+	return movie.NewBuilder().
+		    WithID(id).
+            WithTitle(title).
+            WithYear(uint16(parsedYear)).
+            WithSlogan(slogan).
+            WithResume(resume).
+            WithCoverURL(coverURL).
+			WithDirectors(_directors).
+			WithGenres(_genres).
+			WithOscars(_oscars).
+            Build()
+}
+
